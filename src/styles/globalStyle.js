5952a6e188939cf6daa71999319f0ce4001e64d8
Erick Alves <e7alves@gmail.com>
2020-10-10 22:56:07 -0300

    Setting the fonts

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,12 +1,22 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Roboto';
+    src: url('../assets/fonts/Roboto-Regular.ttf') format('truetype');
+  }
+  @font-face {
+    font-family: 'Courier_Prime';
+    src: url('../assets/fonts/CourierPrime-Regular.ttf') format('truetype');
+  }
+
   * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
   }
   body {
+    font-family: 'Roboto', sans-serif;
     background-color: ${({ theme }) => theme.background};
     color: ${({ theme }) => theme.text};
   }
